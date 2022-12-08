3f2ae672e028e89fd35cb91597e0b2ad70cbcdb1
yahuza96 <yahuzanurideen@gmail.com>
2022-12-08 14:27:16 -0800

    Update 101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+ #include <unistd.h>                                                                                                                      
+/** 
+ *  * main - Entry
+ *   * Return: Always 1
+ *   */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
-
-	return (1);
+		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+			return (1);
 }
